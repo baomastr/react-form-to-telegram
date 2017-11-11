36baee79a73bdbcd29f6c79315459f965d5401b2
BaoTheMastr <smashsux@gmail.com>
2017-11-11 14:23:52 +0300

    simple form elements added

--- src/pages/Form/FormPage.jsx
+++ src/pages/Form/FormPage.jsx
@@ -3,8 +3,12 @@ import React, {Component} from 'react';
 class FormPage extends Component {
     render() {
         return (
-            // bot token 489838568:AAGIRLvCtIYja2afRrU0nR4jBUaXIB-l-ng
-            <div>test</div>
+            <div>
+                <form action="">
+                    <input type="text"/>
+                    <input type="submit"/>
+                </form>
+            </div>
         );
     }
 }
