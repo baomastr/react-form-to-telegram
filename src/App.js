167227fa1e6954a2f9c69258dc5791a9e6494b3c
BaoTheMastr <smashsux@gmail.com>
2017-11-11 13:00:18 +0300

    added pages, homepage

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import HomePage from './pages/HomePage/HomePage.jsx'
 import logo from './logo.svg';
 import styles from './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className={styles.App}>
-        <header className={styles.header}>
-          <img src={logo} className={styles.logo} alt="logo" />
-          <h1 className={styles.title}>Welcome to React</h1>
-        </header>
-        <p className={styles.intro}>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className={styles.App}>
+                <header className={styles.header}>
+                    <img src={logo} className={styles.logo} alt="logo"/>
+                    <h1 className={styles.title}>Welcome to React</h1>
+                </header>
+                <HomePage />
+                <p className={styles.intro}>
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
