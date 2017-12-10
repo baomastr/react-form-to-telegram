2a0c3b03490736c81cd0b7a2bf356b49cc95c598
BaoTheMastr <smashsux@gmail.com>
2017-12-10 15:30:49 +0300

    form styled

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,18 @@
 import React, { PureComponent } from 'react';
+import Paper from 'material-ui/Paper';
 import Form from '../Form/FormPage.jsx'
+
+import s from './HomePage.css'
 
 class HomePage extends PureComponent {
     render() {
         return (
-            <div>
-                <Form />
+            <div className={s.wrapper}>
+                <Paper zDepth={2} >
+                    <div className={s.inner}>
+                        <Form />
+                    </div>
+                </Paper>
             </div>
         );
     }
